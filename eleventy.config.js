d318c54015a9dd9e455a7b1f905768e3eea31e66
Vezquex <admin@vezquex.com>
2025-07-23 07:43:16 -0700

    fix date off by one day

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,8 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc"
 export default async function(c){
+	c.addFilter('dateString', function(date){
+		return date.toISOString().slice(0,10)
+	});
 	c.addPassthroughCopy({'public':'/'})
 	c.setInputDirectory('content')
 	c.addPlugin(pluginWebc, {
